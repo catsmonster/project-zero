cdee5d1ecf1f40ab9655b195c8955ce3b57363eb
catsmonster <shaked.brand@gmail.com>
2019-10-11 09:17:45 +0300

    updated Nav to contain option 1 and option 2

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+      <div>
+          <h2>option 1</h2>
+          <h2>option 2</h2>
+      </div>
+    );
+};
+
+export default Nav;
