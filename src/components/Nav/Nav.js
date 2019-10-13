92e0a536c319ad6484616862c022c07993704e7b
catsmonster <shaked.brand@gmail.com>
2019-10-13 04:00:53 +0300

    fixed fog overlay

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import "./Nav.css";
 
-const Nav = ({onRouteChange, setClassActive, wasClicked, setWasClicked}) => {
+const Nav = ({onRouteChange, setClassActive, wasClicked, setWasClicked, setFinishedLoading1, setFinishedLoading2}) => {
     return (
       <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
           <p className='nav grow' onClick={() => {onRouteChange('first');
           if (wasClicked) {
               setClassActive('state3');
           }
+          setFinishedLoading1(false);
           setWasClicked(true);
           }}>Home</p>
           <p className='nav grow' onClick={() => {onRouteChange('second');
           setWasClicked(true);
+          setFinishedLoading2(false);
           }}>Tables</p>
       </nav>
     );
--- src/components/Second/Second.js
+++ src/components/Second/Second.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import '../First/First.css';
 
-const Second = () => {
+const Second = ({finishedLoading2, setFinishedLoading2}) => {
+    const showFog2 = () => {
+        if (!finishedLoading2) {
+            setTimeout(hideFog, 2000);
+            return (
+                <div>
+                    <div className={`fog4`}>
+                    </div>
+                    <div className={`fog3`}>
+                    </div>
+                </div>
+            );
+        }
+    };
+
+    const hideFog = () => {
+      setFinishedLoading2(true);
+    };
+
     return (
         <div>
-            <div className='fog4'>
-            </div>
-            <div className='fog3'>
-            </div>
+            {showFog2()}
             <div className='newClass'>
             </div>
             <div className='state2'>
